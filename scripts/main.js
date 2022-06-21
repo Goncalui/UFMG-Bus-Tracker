5ac8460707961f45f3c7477cb8e89ce3e6dec634
Goncalui <goncalui27@gmail.com>
2022-06-21 20:07:11 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// let idade = 5;
+// let idade2 = 6;
+// console.log(idade* idade2)
+import * as L from 'leaflet';
+import 'leaflet-polylinedecorator';
+require('leaflet-routing-machine');
+require('lrm-graphhopper'); // This will tack on the class to the L.Routing namespace
+
+    
+  
